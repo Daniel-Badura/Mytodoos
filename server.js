a59ba87e2a3fddbd4ec6fcc9122bd130cb48c92b
Daniel-Badura <daniel.badura@outlook.com>
2021-12-12 02:05:42 +0100

    Set up mongoose

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+
 const app = express();
+
+
+// Connect DB
+connectDB();
+
+// Initialize Middleware
+app.use(express.json({
+    extended: false
+}));
 
 app.get('/', (req, res) =>
     res.json({ msg: 'Welcome o Mytodoos API' })
