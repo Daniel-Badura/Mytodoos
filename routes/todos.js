15e31cf8f3b32d06ac2f05f89d3c7e17cb8fa063
Daniel-Badura <daniel.badura@outlook.com>
2021-12-12 01:19:59 +0100

    Initial express configuration

---
+++ routes/todos.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route   GET api/todos
+// @desc    Get all users todos
+// @access  Private
+router.get('/', (req, res) => {
+    res.send('Get all todos');
+});
+
+// @route   POST api/todos
+// @desc    Add new todo
+// @access  Private
+router.post('/', (req, res) => {
+    res.send('Add todo');
+});
+
+// @route   PUT api/todos/:id
+// @desc    Update existing todo
+// @access  Private
+router.put('/:id', (req, res) => {
+    res.send('Update todo');
+});
+
+// @route   DELETE api/todos/:id
+// @desc    Delete todo
+// @access  Private
+router.delete('/:id', (req, res) => {
+    res.send('Delete todo');
+});
+
+
+module.exports = router;
