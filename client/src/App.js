821df8bc19cd9e370481aa0fc85c61a5e1a8244a
Daniel Badura <daniel.badura@outlook.com>
2022-07-31 19:50:42 +0200

    added about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import Navbar from './components/layout/Navbar';
-import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
-import './App.css';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 const App = () => {
   return (
     <Router>
       <>
         <Navbar />
-        <div className='container'>
+        <div className="container">
           <Switch>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
           </Switch>
         </div>
       </>
